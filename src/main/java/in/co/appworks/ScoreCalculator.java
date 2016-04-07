d7992d248ac0985ee84950e5f81b4141ec502e67
Gaurav.Nagar <A581491@corp.target.com>
2016-04-07 12:26:33 +0530

    Added tests for spare and strike

--- src/main/java/in/co/appworks/ScoreCalculator.java
+++ src/main/java/in/co/appworks/ScoreCalculator.java
@@ -11,10 +11,45 @@ public class ScoreCalculator {
 	public int calculate() {
 		int score = 0;
 
-		for (int i = 0; i < rolls.length; i++) {  
-			score += rolls[i];
+		boolean wasSpare = false;
+		boolean wasStrike = false;
+
+		for (int i = 0; i < rolls.length; i += 2) {
+			int thisFrame = getFrameScore(i);
+			
+			if (wasStrike) {
+				score += thisFrame;
+			} else {
+				if (wasSpare) {
+					score += rolls[i];
+				}
+			}
+
+			score += thisFrame;
+
+			if (isStrike(rolls[i])) {
+				wasStrike = true;
+				i -= 1;
+				wasSpare = false;
+			} else {
+				wasStrike = false;
+				wasSpare = isSpare(rolls[i], rolls[i + 1]);
+			}
 		}
 		return score;
+	}
+
+	private int getFrameScore(int i) {
+		return isStrike(rolls[i]) ? rolls[i] : rolls[i]
+				+ rolls[i + 1];
+	}
+
+	private boolean isStrike(int thisFrame) {
+		return thisFrame == 10;
+	}
+
+	private boolean isSpare(int rollOne, int rollTwo) {
+		return (rollOne + rollTwo) == 10;
 	}
 
 }
--- src/test/java/in/co/appworks/ScoreCalculatorTest.java
+++ src/test/java/in/co/appworks/ScoreCalculatorTest.java
@@ -4,30 +4,58 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import cucumber.api.java.Before;
+
 public class ScoreCalculatorTest {
 
 	@Test
 	public void testThatAGameWithAll1PinsScores20() throws Exception {
-		ScoreCalculator scoreCalculator = new ScoreCalculator();
-
-		scoreCalculator.load(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		int totalScore = calculateScore(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 				1, 1, 1, 1, 1, 1, 1 });
-		
-		int totalScore = scoreCalculator.calculate();
-		
+
 		assertEquals(20, totalScore);
 
 	}
-	
+
 	@Test
 	public void testThatAGameWithAll2PinsScores40() throws Exception {
+		int totalScore = calculateScore(new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+				2, 2, 2, 2, 2, 2, 2 });
+
+		assertEquals(40, totalScore);
+	}
+	
+	@Test
+	public void testThatAGameWith1SpareReturnCorrectScore() throws Exception {
+		int totalScore = calculateScore(new int[] { 5,5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+				2, 2, 2, 2, 2, 2, 2 });
+
+		assertEquals(48, totalScore);
+	}
+	
+	@Test
+	public void testThatAGameWith2SparesReturnCorrectScore() throws Exception {
+		int totalScore = calculateScore(new int[] { 6,4, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+				2, 2, 2, 2, 2, 2, 2 });
+
+		assertEquals(62, totalScore);
+	}
+	
+	@Test
+	public void testThatAGameWith1StrikeReturnCorrectScore() throws Exception {
+		int totalScore = calculateScore(new int[] { 10, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+				2, 2, 2, 2, 2, 2, 2 });
+
+		assertEquals(50, totalScore);
+	}
+	
+
+	private int calculateScore(int[] rolls) {
 		ScoreCalculator scoreCalculator = new ScoreCalculator();
 
-		scoreCalculator.load(new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-				2, 2, 2, 2, 2, 2, 2 });
-		
+		scoreCalculator.load(rolls);
+
 		int totalScore = scoreCalculator.calculate();
-		
-		assertEquals(40, totalScore);
+		return totalScore;
 	}
 }
